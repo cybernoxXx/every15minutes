848235ff8936c4d3331fcc36f5ca88bdf7d194ea
cybernox <francesco.nocentini.92@gmail.com>
2024-05-20 07:56:53 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Loop to print am hours
+for hours in ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
+    for minutes in ['00', '15', '30', '45']:
+        print(str(hours) + ":" + str(minutes) + " am")
+
+# Loop to print pm hours
+for hours in ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
+    for minutes in ['00', '15', '30', '45']:
+        print(str(hours) + ":" + str(minutes) + " pm")
